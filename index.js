2b851955a2760b5cadfe5eca15aeaa714f804c15
bekzat <imashevbekzat77@gmail.com>
2023-02-20 13:34:51 +0300

    second task about cones

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function myName(name, surname) {
   console.log("My name is " + name + surname);
 }
 myName("Bekzat ", " Imashev");
+
+
+function coneVolume(height, radius) {
+  cVolume =  ((height * 3.14 * (radius)  / 2) / 3);
+  console.log("Объем конуса равен - " + cVolume);
+}
+coneVolume(8, 3);
